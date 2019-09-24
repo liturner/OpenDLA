4a2c1b37c0112fcc1db12a68ae5204855793e210
Luke Turner <Luke.Turner@de.bertrandt.com>
2019-09-24 16:09:51 +0200

    started hashing out the Python API

--- OpenDLA/OpenDLA/DLASimulation.h
+++ OpenDLA/OpenDLA/DLASimulation.h
@@ -28,7 +28,6 @@ namespace OpenDLA
 	public:
 		// Very simple for now! Should be more intelegent structure
 		std::vector<Point> m_points;
-
 		std::vector<unsigned int> m_walkers;
 
 	public:
@@ -41,6 +40,20 @@ namespace OpenDLA
 	// Internal Checks
 	private:
 		bool Collides(const Point& _point);
+
+	// The API which python ma
+	private:
+		GetPoints // Returns All Points (possibly with query parameters or filter)
+		PyObject* GetPoint(PyObject* self, PyObject* args);
+		GetNumPoints
+
+		SetPoint // Change an existing point
+		Add Point // Append a new point
+
+		PyObject* AddPoint(PyObject* self, PyObject* args);
+		
+		PyObject* SetWalker(PyObject* self, PyObject* args);
+		PyObject* GetWalker(PyObject* self, PyObject* args);
 
 	// Python Function Calls. These map to calls in the Python files
 	// Mentality is a 1:1 mapping, so what is returned in python will be returned here
